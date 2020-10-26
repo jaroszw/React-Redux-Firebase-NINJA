3794ff64b96ae5a49fc702734b91884ace25e547
Wojciech Jarosz <jaroszw@gmail.com>
2020-10-26 11:51:54 +0100

    initial - before redux instalation

---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ProjectSummary from "./ProjectSummary";
+
+const ProjectList = () => (
+  <div className="project-list section">
+    <ProjectSummary />
+    <ProjectSummary />
+    <ProjectSummary />
+  </div>
+);
+
+export default ProjectList;
